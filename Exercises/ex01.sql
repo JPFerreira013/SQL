4bb4dc4e3714d5ed551d32c21d4f29d1e38ee9b8
Joao Pedro Ferreira <147483818+JPFerreira013@users.noreply.github.com>
2024-03-27 16:40:07 +0100

    Update ex01.sql
    correcao no comentario para o enunciado

--- Exercises/ex01.sql
+++ Exercises/ex01.sql
@@ -1,7 +1,9 @@
-* De la tabla videogames_games, ordenar por año y mostrar el nombre del juego que 
+/* 
+De la tabla videogames_games, ordenar por año y mostrar el nombre del juego que 
 fue publicado por Nintendo en el año 1995 en adelante,
 que no se llame “Donkey Konga 2”, que vendiera entre cero y 0,32 en Europa, 
-entre cero y 0,31 en America, y entre cero y 0,3 en otros territorios… */
+entre cero y 0,31 en America, y entre cero y 0,3 en otros territorios… 
+*/
 
 SELECT * 
 FROM videogames_games 
